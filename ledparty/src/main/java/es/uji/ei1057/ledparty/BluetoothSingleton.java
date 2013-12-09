fd66467da6579b4bbca7bf288021194ffaa96cdf
Óscar Gómez <oscar.gomez@uji.es>
2013-12-09 12:31:33 +0100

    Conexión a socket Bluetooth mejorada. Usando nuevo UUID. Sigue sin conectarse a servidor python

--- ledparty/src/main/java/es/uji/ei1057/ledparty/BluetoothSingleton.java
+++ ledparty/src/main/java/es/uji/ei1057/ledparty/BluetoothSingleton.java
@@ -21,6 +21,11 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by Óscar Gómez <oscar.gomez@uji.es> on 27/11/13.
@@ -35,6 +40,10 @@ public class BluetoothSingleton extends BroadcastReceiver {
     private BluetoothDevice device;
     private List<BluetoothDevice> deviceList = new ArrayList<BluetoothDevice>();
     private DeviceListAdapter listAdapter;
+
+    // UUID y MAC por defecto, para funcionar con el K70IC
+    private String DEFAULT_MAC = "00:0A:3A:7D:66:08";
+    private String DEFAULT_UUID = "0000110a-0000-1000-8000-00805f9b34fb";
 
     public OutputStream outputStream;
     public InputStream inputStream;
@@ -110,9 +119,9 @@ public class BluetoothSingleton extends BroadcastReceiver {
         Toast.makeText(context, "Mostrando lista de dispositivos", Toast.LENGTH_LONG).show();
 
         // Experimento: añadir los dispositivos pareados
-        for (Object bt : bluetooth.getBondedDevices().toArray()) {
-            deviceList.add((BluetoothDevice) bt);
-        }
+//        for (Object bt : bluetooth.getBondedDevices().toArray()) {
+//            deviceList.add((BluetoothDevice) bt);
+//        }
 
         AlertDialog bluetoothDialog = new AlertDialog.Builder(context)
                 .setTitle("Elige un dispositivo")
@@ -126,14 +135,14 @@ public class BluetoothSingleton extends BroadcastReceiver {
                         for (ParcelUuid uuid : uuids) {
                             Log.d("ledparty", "UUID: " + uuid.toString());
                         }
-                        String uuid = uuids[uuids.length - 1].toString();
-                        Toast.makeText(context, "Conectando a dispositivo con UUID " + uuid, Toast.LENGTH_LONG).show();
+                        //String uuid = uuids[uuids.length - 1].toString();
+                        Toast.makeText(context, "Conectando a dispositivo con UUID " + DEFAULT_UUID, Toast.LENGTH_LONG).show();
 
-                        if (connect(uuid)) {
+                        if (connect()) { //connect(uuid)) { // UUID por defecto: 0000110a-0000-1000-8000-00805f9b34fb
                             Intent modesIntent = new Intent(context, ModesActivity.class);
                             context.startActivity(modesIntent);
                         } else {
-                            Toast.makeText(context, "Conexión rechazada!", Toast.LENGTH_SHORT);
+                            Toast.makeText(context, "Conexión rechazada!", Toast.LENGTH_SHORT).show();
                         }
                         ;
 
@@ -148,28 +157,46 @@ public class BluetoothSingleton extends BroadcastReceiver {
      *
      * @return
      */
-    public boolean connect(String uuid) {
-        //Create a Socket connection: need the server's UUID number of registered
-        try {
-            socket = device.createRfcommSocketToServiceRecord(UUID.fromString(uuid));
-            //socket = device.createInsecureRfcommSocketToServiceRecord(UUID.fromString(uuid));
+    public boolean connect(final String uuid) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Callable<Boolean> callable = new Callable<Boolean>() {
+            @Override
+            public Boolean call() {
+                //Create a Socket connection: need the server's UUID number of registered
+                try {
+                    socket = device.createRfcommSocketToServiceRecord(UUID.fromString(uuid));
 
-            socket.connect();
-            Log.d("ledparty", "conectado!");
+                    socket.connect();
+                    Log.d("ledparty", "conectado!");
 
-            inputStream = socket.getInputStream();
-            try {
-                outputStream = socket.getOutputStream();
-            } catch (Exception e) {
-                Log.d("ledparty", "EXCEPCIÓN!!: " + e.getMessage());
+                    try {
+                        inputStream = socket.getInputStream();
+                        outputStream = socket.getOutputStream();
+                    } catch (Exception e) {
+                        Log.d("ledparty", "EXCEPCIÓN!!: " + e.getMessage());
+                    }
+
+                    outputStream.write("AAAAAHHHH!!!".getBytes());
+
+                    return true;
+                } catch (Exception e) {
+                    //e.printStackTrace();
+                    Log.e("ledparty", "excepción en bluetooth.connect()");
+                    return false;
+                }
             }
-
-            outputStream.write("AAAAAHHHH!!!".getBytes());
-
-            return true;
-        } catch (Exception e) {
+        };
+        Future<Boolean> future = executor.submit(callable);
+        boolean exito = false;
+        try {
+            exito = future.get();
+        } catch (InterruptedException e) {
             e.printStackTrace();
-            return false;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+            return exito;
         }
     }
 
@@ -180,7 +207,7 @@ public class BluetoothSingleton extends BroadcastReceiver {
      * @return
      */
     public boolean connect() {
-        return connect("00001101-0000-1000-8000-00805F9B34FB");
+        return connect(DEFAULT_UUID);
     }
 
     /**
